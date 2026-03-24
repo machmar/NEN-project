77eb6e64840752adb54b2e576ea7b8b0338f603e
Marek Mach <marekmach24@gmail.com>
2026-03-24 14:48:56 +0100

    Added a fixed point library

---
+++ fx8.h
@@ -0,0 +1,66 @@
+/* 
+ * File:   fx8.h
+ * Author: 247022
+ *
+ * Created on 24. b?ezna 2026, 14:13
+ */
+
+#ifndef FX8_H
+#define FX8_H
+
+#include <stdint.h>
+
+typedef int16_t fx_t;
+
+/* 8.8 fixed-point basics */
+#define FX_SHIFT        8
+#define FX_ONE          ((fx_t)256)
+#define FX_HALF         ((fx_t)128)
+#define FX_TWO          ((fx_t)512)
+
+#define FX_FROM_INT(x)  ((fx_t)((x) << FX_SHIFT))
+#define FX(x)           (FX_FROM_INT(x))
+#define FX_TO_INT(x)    ((int16_t)((x) >> FX_SHIFT))
+#define FX_I(x)         (FX_TO_INT(x))
+#define FX_FRAC(x)      ((uint8_t)((x) & 0xFF))
+#define FX_RAW(x)       ((fx_t)(x))
+
+/* angle unit: full turn = 2.0 = 512 raw */
+#define FX_ANGLE_FULL   ((fx_t)512)
+#define FX_ANGLE_HALF   ((fx_t)256)
+#define FX_ANGLE_QUAD   ((fx_t)128)
+
+/* fast ops */
+static inline fx_t fx_add(fx_t a, fx_t b) { return (fx_t)(a + b); }
+static inline fx_t fx_sub(fx_t a, fx_t b) { return (fx_t)(a - b); }
+static inline fx_t fx_neg(fx_t a)         { return (fx_t)(-a); }
+static inline fx_t fx_abs(fx_t a)         { return (a < 0) ? (fx_t)(-a) : a; }
+
+/* 8.8 * 8.8 -> 8.8 */
+static inline fx_t fx_mul(fx_t a, fx_t b)
+{
+    return (fx_t)(((int32_t)a * (int32_t)b) >> FX_SHIFT);
+}
+
+/* 8.8 / 8.8 -> 8.8 */
+static inline fx_t fx_div(fx_t a, fx_t b)
+{
+    return (fx_t)(((int32_t)a << FX_SHIFT) / b);
+}
+
+/* integer helpers */
+static inline fx_t fx_mul_i(fx_t a, int8_t b)
+{
+    return (fx_t)(a * b);
+}
+
+static inline fx_t fx_div_i(fx_t a, int8_t b)
+{
+    return (fx_t)(a / b);
+}
+
+/* trig */
+fx_t fx_sin(fx_t a);
+fx_t fx_cos(fx_t a);
+
+#endif
